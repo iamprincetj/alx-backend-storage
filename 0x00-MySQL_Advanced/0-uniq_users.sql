7ebc7640b6a50e6b3ac3ca23a37b465c9e499f5e
iamprincetj <tochukwunwanze5@gmail.com>
2023-10-11 16:16:35 +0100

    0-uniq_users.sql done

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,10 @@
+-- A SQL script that creates a table users
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
